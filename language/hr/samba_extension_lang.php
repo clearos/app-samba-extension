ad5bdd375af6c870274c0f47524c66f2ee95bd68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:00:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba Proširenje proteže imenik sa atributima potrebnim za Windows umrežavanje.';
+$lang['samba_extension_app_name'] = 'Samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba Proširenje račun';
+$lang['samba_extension_windows_networking'] = 'Windows mreže';
