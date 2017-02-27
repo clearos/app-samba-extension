f7c0e0f7aeaa0dbfb23d2c5bcb32ebdd7aaf2509
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:11:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'די סאַמבאַ געשפּרייט יקסטענדז די דירעקטארי מיט אַטראַביוץ required פֿאַר ווינדאָוז נעטוואָרקינג.';
+$lang['samba_extension_app_name'] = 'סאַמבאַ עקסטענסיאָן';
+$lang['samba_extension_samba_account_extension'] = 'סאַמבאַ אַקאַונט עקסטענסיאָן';
+$lang['samba_extension_windows_networking'] = 'פֿענצטער נעטוואָרקינג';
