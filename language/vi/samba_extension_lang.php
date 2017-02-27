7c9a9e9b7e213571610a47d0ca2a389953fc0e7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:11:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba mở rộng mở rộng thư mục với các thuộc tính cần thiết cho Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba mở rộng';
+$lang['samba_extension_samba_account_extension'] = 'Gia hạn tài khoản Samba';
+$lang['samba_extension_windows_networking'] = 'kết nối mạng windows';
