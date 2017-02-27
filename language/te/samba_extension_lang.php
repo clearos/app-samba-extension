3d88b752797514fbc0a452c72bc416bfced713d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:09:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'సాంబా పొడిగింపు విండోస్ నెట్వర్కింగ్ కోసం అవసరమైన లక్షణాలను తో డైరెక్టరీ విస్తరించింది.';
+$lang['samba_extension_app_name'] = 'సాంబా పొడిగింపు';
+$lang['samba_extension_samba_account_extension'] = 'సాంబా ఖాతా పొడిగింపు';
+$lang['samba_extension_windows_networking'] = 'విండోస్ నెట్వర్కింగ్';
