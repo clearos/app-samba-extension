5581b4731ead66d22e3ecd9a5bbb6fe5f7d63ca6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:01:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'સામ્બા એક્સ્ટેંશન Windows નેટવર્કીંગ માટે જરૂરી લક્ષણો સાથે ડિરેક્ટરી લંબાય છે.';
+$lang['samba_extension_app_name'] = 'સામ્બા એક્સ્ટેંશન';
+$lang['samba_extension_samba_account_extension'] = 'સામ્બા એકાઉન્ટ એક્સ્ટેંશન';
+$lang['samba_extension_windows_networking'] = 'Windows નેટવર્કીંગ';
