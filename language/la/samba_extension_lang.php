3520f75ca344c5e1dc3a7010f8b173ef4f41b1b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:04:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba est extensio usque in Directory in passionibus pro Fenestra Networking requiritur.';
+$lang['samba_extension_app_name'] = 'Samba extensio';
+$lang['samba_extension_samba_account_extension'] = 'Samba Account extensio';
+$lang['samba_extension_windows_networking'] = 'Software in Fenestra';
