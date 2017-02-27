6fbbcd7419bda45fcfd7f9ae65280f13cc3ffc18
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:03:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'ផ្នែកបន្ថែមរបស់ Samba ពង្រីកថតដែលមានគុណលក្ខណៈដែលបានទាមទារសម្រាប់ Windows ណ្តាញ។';
+$lang['samba_extension_app_name'] = 'ផ្នែកបន្ថែម Samba & ‧;';
+$lang['samba_extension_samba_account_extension'] = 'ផ្នែកបន្ថែមគណនី Samba & ‧;';
+$lang['samba_extension_windows_networking'] = 'បណ្តាញប្រព័ន្ធប្រតិបត្តិការ Windows';
