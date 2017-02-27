dea15dfcc0881869ce82673294e7321afc05d76f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:59:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'The Samba Extension amawapatsa Directory ndi makhalidwe wofunikila Windows Intaneti.';
+$lang['samba_extension_app_name'] = 'Samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba Account Extension';
+$lang['samba_extension_windows_networking'] = 'Windows Intaneti';
