ce08070ad6e1cf9dab4a0962f427a2313ac06010
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:03:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba Extension framlengir skrá með eiginleika sem þarf til Windows Networking.';
+$lang['samba_extension_app_name'] = 'samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba Reikningur Extension';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
