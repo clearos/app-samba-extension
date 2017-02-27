a16dacc0aa6774279fefc3035800db1930bcad60
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:09:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Nu Samba penyuluh ngalegaan nu diréktori jeung atribut diperlukeun pikeun Windows jaringan.';
+$lang['samba_extension_app_name'] = 'Samba penyuluh';
+$lang['samba_extension_samba_account_extension'] = 'Samba Akun penyuluh';
+$lang['samba_extension_windows_networking'] = 'Windows jaringan';
