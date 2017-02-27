0a26db2c6289d8338f3e2b3d343de720ef68adc2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:09:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'சம்பா நீட்டிப்பு விண்டோஸ் வலையமைப்பு தேவையான பண்புகளை கொண்ட அடைவு பரவியுள்ளது.';
+$lang['samba_extension_app_name'] = 'சம்பா நீட்டிப்பு';
+$lang['samba_extension_samba_account_extension'] = 'சம்பா கணக்கு நீட்டிப்பு';
+$lang['samba_extension_windows_networking'] = 'விண்டோஸ் வலையமைப்பு';
