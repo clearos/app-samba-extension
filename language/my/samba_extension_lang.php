4ca66ceecf58822a7d6f1fc2c87b95838ae43e9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:59:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'အဆိုပါ Samba တိုးချဲ့မှုကို Windows ကွန်ယက်အဖွဲ့များအတွက်လိုအပ်သော Attribute တွေနှင့်အတူ directory ကိုချဲ့ထွင်။';
+$lang['samba_extension_app_name'] = 'Samba တိုးချဲ့မှု';
+$lang['samba_extension_samba_account_extension'] = 'Samba အကောင့်တိုးချဲ့မှု';
+$lang['samba_extension_windows_networking'] = 'Windows ကိုကွန်ယက်အဖွဲ့';
