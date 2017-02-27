4c6b01b84e044b4caf26ee8ce00fbe776b7f523a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:05:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Te Samba Extension atu te whaiaronga ki ngā huanga e hiahiatia ana mō te Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Toronga';
+$lang['samba_extension_samba_account_extension'] = 'Samba Pūkete Toronga';
+$lang['samba_extension_windows_networking'] = 'Networking Windows';
