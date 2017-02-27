e04ed79985ed5f41360b46d18dac2586d6b85651
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:06:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'The Samba Extension memanjangkan direktori dengan sifat-sifat yang diperlukan untuk Windows Rangkaian.';
+$lang['samba_extension_app_name'] = 'Samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba Akaun Extension';
+$lang['samba_extension_windows_networking'] = 'Windows Rangkaian';
