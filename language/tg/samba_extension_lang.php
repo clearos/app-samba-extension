9ff538bc123566f4473e74133bf23f62dbfc9785
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:09:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Дар Samba Тамдиди тамдид феҳрист бо хусусиятҳои барои Networking Windows.';
+$lang['samba_extension_app_name'] = 'Samba Тамдиди';
+$lang['samba_extension_samba_account_extension'] = 'Samba Ҳисоби Тамдиди';
+$lang['samba_extension_windows_networking'] = 'Networking Windows';
