6a3806f172e95edeef84729fddcdc7cfa2678d5f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:11:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'The Samba Extension usinika lwemibhalo izibaluli ezidingekayo for Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba-Akhawunti Extension';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
