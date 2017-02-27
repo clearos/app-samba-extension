99f9616e39fc5d717555d85e68d2dd2fd93bd8c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:10:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'سامبا توسیع ونڈوز نیٹ ورکنگ کے لئے ضروری صفات کے ساتھ ڈائرکٹری میں توسیع.';
+$lang['samba_extension_app_name'] = 'سامبا توسیع';
+$lang['samba_extension_samba_account_extension'] = 'سامبا اکاؤنٹ توسیع';
+$lang['samba_extension_windows_networking'] = 'ونڈوز نیٹ ورکنگ';
