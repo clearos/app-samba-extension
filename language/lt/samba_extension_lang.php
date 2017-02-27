c8eab3fa1d27e4474f1a57fa3353a726eab9cd65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:04:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba pratęsimas tęsiasi katalogą su atributais, reikalingų Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba pratęsimas';
+$lang['samba_extension_samba_account_extension'] = 'Samba paskyra pratęsimas';
+$lang['samba_extension_windows_networking'] = '"Windows tinklai';
