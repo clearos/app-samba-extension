4731bd328e2c193773a644853642b6246374ca7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:00:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'La Samba Pligrandigo etendas la dosierujo kun atributoj postulataj por Windows Retoj.';
+$lang['samba_extension_app_name'] = 'sambo Pligrandigo';
+$lang['samba_extension_samba_account_extension'] = 'Sambo Konto Pligrandigo';
+$lang['samba_extension_windows_networking'] = 'fenestroj Interkonektanta';
