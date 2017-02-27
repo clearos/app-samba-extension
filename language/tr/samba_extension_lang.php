b748a5f5ca6cf60779ae7f6e209ef0783a345485
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:10:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba Uzatma Windows Networking için gerekli niteliklere sahip dizini uzanır.';
+$lang['samba_extension_app_name'] = 'Samba Uzantısı';
+$lang['samba_extension_samba_account_extension'] = 'Samba Hesap Uzantısı';
+$lang['samba_extension_windows_networking'] = 'Windows ağ';
