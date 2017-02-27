04372eff0d850ce3fb2ee377623b278d46016c98
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:04:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = '삼바 확장은 Windows 네트워킹에 필요한 속성을 가진 디렉토리를 확장합니다.';
+$lang['samba_extension_app_name'] = '삼바 확장';
+$lang['samba_extension_samba_account_extension'] = '삼바 계정 확장';
+$lang['samba_extension_windows_networking'] = 'Windows 네트워킹';
