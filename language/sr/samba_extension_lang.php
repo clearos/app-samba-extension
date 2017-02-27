fdc544d1a072e2d17512aa64873c1bd32e7f1de9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:07:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Самба Продужење проширује директоријум са атрибутима потребним за Виндовс умрежавање.';
+$lang['samba_extension_app_name'] = 'самба Проширење';
+$lang['samba_extension_samba_account_extension'] = 'Самба налог Проширење';
+$lang['samba_extension_windows_networking'] = 'виндовс Умрежавање';
