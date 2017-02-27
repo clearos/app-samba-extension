f777d66131dc3b790a0574df508393bba6970b08
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:02:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'A Samba Extension kiterjeszti a könyvtár attribútumok szükséges a Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba Fiókbővítmény';
+$lang['samba_extension_windows_networking'] = 'A windows hálózati';
