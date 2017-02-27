bf5f7e339d896ddd0e02d805df0d8701362c5dac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:10:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'ส่วนต่อขยายของแซมบ้าขยายไดเรกทอรีที่มีคุณลักษณะที่จำเป็นสำหรับ Windows เครือข่าย';
+$lang['samba_extension_app_name'] = 'ขยายแซมบ้า';
+$lang['samba_extension_samba_account_extension'] = 'ส่วนขยายบัญชีแซมบ้า';
+$lang['samba_extension_windows_networking'] = 'เครือข่ายของ windows';
