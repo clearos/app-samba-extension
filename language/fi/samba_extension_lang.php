49f873f99aff2010a129ab290590fc57aab41054
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:01:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba Extension ulottuu hakemistoon ominaisuuksia tarvita Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba Tilin Extension';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
