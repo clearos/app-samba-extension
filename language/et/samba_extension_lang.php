3ddf618888c1cadcd992997c692ab11525848fc8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:00:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba pikendamine pikendab kataloogi atribuudid vajalik Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba pikendamine';
+$lang['samba_extension_samba_account_extension'] = 'Samba Kontolaiend';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
