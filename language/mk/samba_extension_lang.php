601febe2ef69743707da4c93cb009f0bbe2036ab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:05:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Самба Продолжување протега директориумот со кои се бараат за Windows вмрежување.';
+$lang['samba_extension_app_name'] = 'Самба Продолжување';
+$lang['samba_extension_samba_account_extension'] = 'Самба сметка Продолжување';
+$lang['samba_extension_windows_networking'] = 'Windows вмрежување';
