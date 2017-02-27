be4763a6a3bfdd32f35ebd8d87d8aa6f6f78b2f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:58:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'سامبا تمديد يمتد الدليل مع السمات المطلوبة للشبكات ويندوز.';
+$lang['samba_extension_app_name'] = 'سامبا التمديد';
+$lang['samba_extension_samba_account_extension'] = 'تمديد سامبا حساب';
+$lang['samba_extension_windows_networking'] = 'شبكة Windows';
