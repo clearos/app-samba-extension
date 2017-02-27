1b1918338480e986bdc2f534371f3f9789fe7a78
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:05:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba Өргөтгөл Windows Сүлжээ шаардлагатай шинж санг өргөтгөн.';
+$lang['samba_extension_app_name'] = 'Samba Өргөтгөл';
+$lang['samba_extension_samba_account_extension'] = 'Samba Дансны Өргөтгөл';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
