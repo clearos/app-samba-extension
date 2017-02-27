2a5dad231f8c91a122b9f4a4a3f4ee9875f59bd4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:02:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'An Leathnú Samba Síneann an t-eolaire a bhfuil tréithe atá riachtanach do Windows Líonrú.';
+$lang['samba_extension_app_name'] = 'Síneadh samba';
+$lang['samba_extension_samba_account_extension'] = 'Síneadh Cuntas samba';
+$lang['samba_extension_windows_networking'] = 'Windows Líonrú';
