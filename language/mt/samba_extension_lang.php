c7ebc3f9c39143ba0c6381e6680d8aae1f6c1117
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:06:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Il Estensjoni Samba testendi l-direttorju ma &#39;attributi meħtieġa għall-Windows Netwerking.';
+$lang['samba_extension_app_name'] = 'Samba Estensjoni';
+$lang['samba_extension_samba_account_extension'] = 'Samba Kont Estensjoni';
+$lang['samba_extension_windows_networking'] = 'twieqi Netwerking';
