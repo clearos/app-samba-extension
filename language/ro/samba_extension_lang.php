41e45e6ccc385c1cf4a591ab4baca6641396289b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:07:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Extensia Samba extinde directorul cu atributele necesare pentru Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Extensie';
+$lang['samba_extension_samba_account_extension'] = 'Samba Extensie cont';
+$lang['samba_extension_windows_networking'] = 'ferestre din rețea';
