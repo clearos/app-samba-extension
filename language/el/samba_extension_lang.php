f3097c0f72283656623c5f82408f4a287d092d2f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:01:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Η Samba Επέκταση επεκτείνει τον κατάλογο με τα χαρακτηριστικά που απαιτούνται για τα Windows δικτύωσης.';
+$lang['samba_extension_app_name'] = 'Samba Επέκταση';
+$lang['samba_extension_samba_account_extension'] = 'Samba Παράταση Λογαριασμού';
+$lang['samba_extension_windows_networking'] = 'Δικτύωση των windows';
