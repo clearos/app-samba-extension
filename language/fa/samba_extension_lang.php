1da1901b5619e80137d1938ab6018f46c2b011c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:07:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'سامبا فرمت گسترش دایرکتوری با ویژگی های مورد نیاز برای شبکه های ویندوز.';
+$lang['samba_extension_app_name'] = 'سامبا فرمت';
+$lang['samba_extension_samba_account_extension'] = 'سامبا حساب فرمت';
+$lang['samba_extension_windows_networking'] = 'شبکه های ویندوز';
