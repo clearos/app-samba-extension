0dd1c6e96442db21210c8bf329d7fb2891324797
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:58:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba Extension shtrihet në dosjen me atributet e nevojshme për Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba Llogaria Extension';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
