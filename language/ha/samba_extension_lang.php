9d5922022050da2bae872bc3f66df8a0c8367687
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:02:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'The Samba Tsawo kara shugabanci da halayen da ake bukata domin Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Tsawo';
+$lang['samba_extension_samba_account_extension'] = 'Samba Account Tsawo';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
