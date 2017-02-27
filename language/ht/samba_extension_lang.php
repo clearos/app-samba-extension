44a4744cf1624c14df9b0fd37eb15e8c2580dd20
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:02:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Ekstansyon an Samba fin anyè a ak atribi figi mande pou Windows Networking.';
+$lang['samba_extension_app_name'] = 'Samba Ekstansyon';
+$lang['samba_extension_samba_account_extension'] = 'Samba Kont Ekstansyon';
+$lang['samba_extension_windows_networking'] = 'Windows Rezo';
