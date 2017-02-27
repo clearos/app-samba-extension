dfa819825b18b4f80eb0316712250312ed965636
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:04:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'ການຂະຫຍາຍ Samba ຂະຫຍາຍລະບົບທີ່ມີຄຸນລັກສະນະທີ່ຕ້ອງການສໍາລັບ Windows ລະບົບເຄືອຂ່າຍ.';
+$lang['samba_extension_app_name'] = 'ການຂະຫຍາຍ Samba';
+$lang['samba_extension_samba_account_extension'] = 'ການຂະຫຍາຍບັນຊີ Samba';
+$lang['samba_extension_windows_networking'] = 'ເຄືອຂ່າຍ Windows';
