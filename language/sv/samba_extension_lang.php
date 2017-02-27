295d443b794e45182ab844c4a3daeba54aae3820
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:09:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba Extension utökar katalogen med attribut som krävs för Windows-nätverk.';
+$lang['samba_extension_app_name'] = 'samba Extension';
+$lang['samba_extension_samba_account_extension'] = 'Samba konto Extension';
+$lang['samba_extension_windows_networking'] = 'Windows-nätverk';
