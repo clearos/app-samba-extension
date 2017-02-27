b963395ad51cfb11a44d752ba4d10d97a560ba15
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:03:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba ұзарту Windows Желі үшін қажетті атрибуттары бар каталогты кеңейтеді.';
+$lang['samba_extension_app_name'] = 'Samba ұзарту';
+$lang['samba_extension_samba_account_extension'] = 'Samba шот ұзарту';
+$lang['samba_extension_windows_networking'] = 'Windows Networking';
