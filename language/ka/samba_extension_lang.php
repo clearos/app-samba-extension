87edd993b467b09acf93929204a578c34a0c619f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:03:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'სამბა გაფართოება ვრცელდება დირექტორია ატრიბუტები საჭირო Windows ქსელში.';
+$lang['samba_extension_app_name'] = 'Samba გაფართოება';
+$lang['samba_extension_samba_account_extension'] = 'Samba ანგარიშის გაფართოება';
+$lang['samba_extension_windows_networking'] = 'Windows ქსელში';
