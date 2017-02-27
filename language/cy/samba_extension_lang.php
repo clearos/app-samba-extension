089c5036323799abd35cd4268007298d46b5a5bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:11:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Mae&#39;r Estyniad Samba yn ymestyn cyfeiriadur gyda nodweddion sy&#39;n ofynnol ar gyfer Windows Rhwydweithio.';
+$lang['samba_extension_app_name'] = 'Samba Estyniad';
+$lang['samba_extension_samba_account_extension'] = 'Estyniad Cyfrif Samba';
+$lang['samba_extension_windows_networking'] = 'Windows Rhwydweithio';
