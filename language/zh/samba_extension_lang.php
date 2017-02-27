94717b2a376dfe16956b379eec92fd0c7b7d7e30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:00:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/samba_extension_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['samba_extension_app_description'] = 'Samba的扩展与延伸为Windows网络所需的属性的目录。';
+$lang['samba_extension_app_name'] = '桑巴扩展';
+$lang['samba_extension_samba_account_extension'] = '桑巴帐户扩展';
+$lang['samba_extension_windows_networking'] = 'Windows网络';
